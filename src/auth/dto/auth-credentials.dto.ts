7935daebef81ee2743f6dd4ddb4999be352cb72b
MelinaMontes <you@example.com>
2022-02-06 14:17:48 -0300

    creating a user

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    username: string;
+
+  //Passwords will contain at least 1 upper case letter
+  //Passwords will contain at least 1 lower case letter
+  //Passwords will contain at least 1 number or special character
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(15)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+      message:  'Your password is too weak! Try again', 
+    })
+    password: string;
+
+
+}
